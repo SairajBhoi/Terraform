465afa8dba9d8d5ca5633b9e048e18ebb5537ba8
Sairaj B <sairaj.m.bhoi@intimetec.com>
2023-05-03 22:51:03 +0530

    commented Ansible part od terraform ec2 module

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -16,15 +16,15 @@ provisioner "local-exec" {
   EOT
 }
 
-provisioner "local-exec" {
-  command = <<-EOT
-       echo "week ansible_host='${aws_instance.remote.public_ip}' ansible_connection=ssh ansible_user=ubuntu" >> /root/terraform/rst/inventory.ini
-  EOT
-}
-provisioner "local-exec" {
-  command = <<-EOT
-      ansible-playbook -i /root/terraform/rst/inventory.ini --private-key=~/.ssh/key.pem playbookweek.yaml
-  EOT
-}
+# provisioner "local-exec" {
+#   command = <<-EOT
+#        echo "week ansible_host='${aws_instance.remote.public_ip}' ansible_connection=ssh ansible_user=ubuntu" >> /root/terraform/rst/inventory.ini
+#   EOT
+# }
+# provisioner "local-exec" {
+#   command = <<-EOT
+#       ansible-playbook -i /root/terraform/rst/inventory.ini --private-key=~/.ssh/key.pem playbookweek.yaml
+#   EOT
+# }
 }
 
--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "remote-locate-terraform-state-bucket"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "remote-locate-terraform-state-bucket"
+#     key    = "terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
 
